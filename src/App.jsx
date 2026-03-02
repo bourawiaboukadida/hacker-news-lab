1eaea772ef884352103dfd9e03ceb2070a613a58
bourawiaboukadida <bourawiaboukadida422@gmail.com>
2026-03-02 14:28:07 +0100

    Week 4: Extracting components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const stories = [
+  {
+    title: 'React',
+    url: 'https://react.dev/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Vite',
+    url: 'https://vite.dev/',
+    author: 'Evan You',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
+function List() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {stories.map((item) => (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span> {item.author} </span>
+          <span> {item.num_comments} </span>
+          <span> {item.points} </span>
+        </div>
+      ))}
     </>
-  )
+  );
+}
+function App() {
+  return (
+    <div>
+      <h1>Hacker News</h1>
+      <List />
+    </div>
+  );
 }
 
-export default App
+export default App;
